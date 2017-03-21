9b263aecb18c434557f586f771ff988740846dc9
Ádám Horváth <horada1991@gmail.com>
2017-03-21 12:27:26 +0100

    Remove lombok because of travis CI...

--- src/main/java/com/codecool/neighbrotaxi/model/entities/Route.java
+++ src/main/java/com/codecool/neighbrotaxi/model/entities/Route.java
@@ -1,13 +1,11 @@
 package com.codecool.neighbrotaxi.model.entities;
 
 import com.codecool.neighbrotaxi.model.GeoCoord;
-import lombok.Data;
 
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Data
 public class Route {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -19,4 +17,44 @@ public class Route {
 
     @ManyToOne
     private Car car;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public GeoCoord getStart() {
+        return start;
+    }
+
+    public void setStart(GeoCoord start) {
+        this.start = start;
+    }
+
+    public GeoCoord getDestination() {
+        return destination;
+    }
+
+    public void setDestination(GeoCoord destination) {
+        this.destination = destination;
+    }
+
+    public Date getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(Date departure) {
+        this.departure = departure;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
 }
