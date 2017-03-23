efe5b05af01bba0f7212bcbd727a1fcdd0823a96
Ádám Horváth <horada1991@gmail.com>
2017-03-23 12:50:10 +0100

    RouteServiceImpl saveNewRoute method with tests and doc

---
+++ src/main/java/com/codecool/neighbrotaxi/service/RouteService.java
@@ -0,0 +1,8 @@
+package com.codecool.neighbrotaxi.service;
+
+
+import com.codecool.neighbrotaxi.model.entities.Route;
+
+public interface RouteService {
+    public void saveNewRoute(Route route);
+}
