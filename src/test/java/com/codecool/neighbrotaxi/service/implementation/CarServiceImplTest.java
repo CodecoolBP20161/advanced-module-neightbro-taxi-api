90050746314294395f6f6c30b6bc51087b180dd3
Ádám Horváth <horada1991@gmail.com>
2017-03-24 10:55:23 +0100

    CarService impl+test+doc

---
+++ src/test/java/com/codecool/neighbrotaxi/service/implementation/CarServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.codecool.neighbrotaxi.service.implementation;
+
+import com.codecool.neighbrotaxi.AbstractTest;
+import com.codecool.neighbrotaxi.model.entities.Car;
+import com.codecool.neighbrotaxi.repository.CarRepository;
+import com.codecool.neighbrotaxi.service.CarService;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import javax.transaction.Transactional;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.when;
+
+@Transactional
+@MockBean(CarRepository.class)
+public class CarServiceImplTest extends AbstractTest {
+    @Autowired
+    private CarRepository carRepository;
+
+    @Autowired
+    private CarService carService;
+
+    private Car car;
+
+    @Before
+    public void setUp() throws Exception {
+        car = new Car();
+    }
+
+    @Test
+    public void findOne_ReturnValidObject() throws Exception {
+        when(carRepository.findOne(anyInt())).thenReturn(car);
+
+        Object returnedObject = carService.findOne(anyInt());
+
+        assertEquals(car, returnedObject);
+    }
+
+}
