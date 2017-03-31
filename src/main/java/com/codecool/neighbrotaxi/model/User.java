db4a20092a961cb8776e33578c6839258b2de9a3
kalmeee <revkalman@gmail.com>
2017-03-31 15:10:54 +0200

    setter and getter for car entity

--- src/main/java/com/codecool/neighbrotaxi/model/User.java
+++ src/main/java/com/codecool/neighbrotaxi/model/User.java
@@ -92,4 +92,12 @@ public class User {
     public String getPhoneNumber() {
         return phoneNumber;
     }
+
+    public Set<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(Set<Car> cars) {
+        this.cars = cars;
+    }
 }
